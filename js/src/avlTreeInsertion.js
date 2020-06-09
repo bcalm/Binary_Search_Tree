56e8ac8262a0b7efcae9658ecf081b5cc4577405
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-09 12:35:52 +0530

    Added avlTree deletion method in js.

--- js/src/avlTreeInsertion.js
+++ js/src/avlTreeInsertion.js
@@ -39,7 +39,7 @@ const getHeightDiff = function (tree) {
   if (!tree) {
     return 0;
   }
-  return Math.abs(height(tree.left) - height(tree.right));
+  return height(tree.left) - height(tree.right);
 };
 
 const insert = function (tree, value) {
@@ -54,7 +54,6 @@ const insert = function (tree, value) {
   }
 
   tree.height = Math.max(height(tree.left), height(tree.right)) + 1;
-
   const heightDiff = getHeightDiff(tree);
 
   if (heightDiff < -1 && value > tree.right.value) {
@@ -74,13 +73,55 @@ const insert = function (tree, value) {
     tree.right = rightRotate(tree.right);
     return leftRotate(tree);
   }
+  return tree;
+};
 
+const find_min = function (tree) {
+  if (tree.left === null) {
+    return tree;
+  }
+  return find_min(tree.left);
+};
+
+const deleteNode = function (tree, value) {
+  if (tree == null) return tree;
+
+  if (value < tree.value) tree.left = deleteNode(tree.left, value);
+  else if (value > tree.value) tree.right = deleteNode(tree.right, value);
+  else {
+    if (tree.left == null) return tree.right;
+    if (tree.right == null) return tree.left;
+    const minNode = find_min(tree.right);
+    tree.value = minNode.value;
+    tree.right = deleteNode(tree.right, minNode.value);
+  }
+
+  tree.height = 1 + Math.max(height(tree.left), height(tree.right));
+
+  const heightDiff = getHeightDiff(tree);
+  if (heightDiff > 1 && getHeightDiff(tree.left) >= 0) {
+    return rightRotate(tree);
+  }
+
+  if (heightDiff > 1 && getHeightDiff(tree.left) < 0) {
+    tree.left = leftRotate(tree.left);
+    return rightRotate(tree);
+  }
+
+  if (heightDiff < -1 && getHeightDiff(tree.right) <= 0) {
+    return leftRotate(tree);
+  }
+
+  if (heightDiff < -1 && getHeightDiff(tree.right) > 0) {
+    tree.right = rightRotate(tree.right);
+    return leftRotate(tree);
+  }
   return tree;
 };
 
 const main = function () {
-  const values = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-  const tree = values.reduce(insert, null);
+  const values = [15, 25, 20, 8, 1, 5, 10];
+  let tree = values.reduce(insert, null);
   console.log(JSON.stringify(tree));
   return 0;
 };
